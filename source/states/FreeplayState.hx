41e5ac0d76fb622ad516574b306458cf3a402602
StarNovaBR <cherrylegal001@gmail.com>
2026-04-01 10:10:44 -0300

    okay... la vamos nos

--- source/states/FreeplayState.hx
+++ source/states/FreeplayState.hx
@@ -270,11 +270,11 @@ class FreeplayState extends MusicBeatState
 						if (!Highscore.getWeekCompletion("2wiik2")) song.locked = true;
 					case "miisacre" | "lazulii" | "snacks":
 						if (!Highscore.getWeekCompletion("3wiikZ")) song.locked = true;
-					case "illusion" | "heavenfall" | "long awaited" | "god mode" | "broadcasting":
-						//if (!Highscore.getWeekCompletion("4wiikFisticuffs")) song.locked = true;
+					case "long awaited" | "god mode" | "broadcasting":
+						if (!Highscore.getWeekCompletion("4wiikFisticuffs")) song.locked = true;
 					case "foulplay":
 						var unlocked = true;
-						for (song in ["snacks", "long-awaited", "god-mode", "broadcasting", "lazulii", "illusion", "heavenfall"]) {
+						for (song in ["snacks", "long-awaited", "god-mode", "broadcasting", "lazulii"]) {
 							if (!Highscore.getCompletion(song)) {
 								unlocked = false;
 							}
