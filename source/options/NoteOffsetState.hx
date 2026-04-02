f1bd21e6d5cd7201f6dffbeeffcef4ba7e3246ed
StarNovaBR <cherrylegal001@gmail.com>
2026-04-02 12:10:29 -0300

    porra

--- source/options/NoteOffsetState.hx
+++ source/options/NoteOffsetState.hx
@@ -393,7 +393,7 @@ class NoteOffsetState extends MusicBeatState
 				}
 			}
 
-			if(controls.RESET #if mobile virtualPad.buttonC.justPressed #end)
+			if(controls.RESET #if mobile || virtualPad.buttonC.justPressed #end)
 			{
 				for (i in 0...ClientPrefs.data.comboOffset.length)
 				{
